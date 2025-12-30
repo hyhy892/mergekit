95508fae85808f3c5ca198007898a793401b928f
hyhy892 <zhanghuiyi_dut@163.com>
2025-12-31 02:12:51 +0800

    add simple scripts

---
+++ run_model.sh
@@ -0,0 +1,14 @@
+# CUDA_VISIBLE_DEVICES=0,1,2,3 mergekit-tokensurgeon \
+#   /ssd4/lizijian/Models/DeepSeek-R1-Distill-Qwen-32B \
+#   /ssd4/baohangli/ModelsHub/Qwen/Qwen3-32B \
+#   /ssd4/hyzhang/Kit/output_model/Deepseek_to_Qwen3
+
+# CUDA_VISIBLE_DEVICES=0,1,2,3 mergekit-tokensurgeon \
+#   /ssd4/hyzhang/MODELS/Ministral-3-14B-Instruct-2512 \
+#   /ssd4/baohangli/ModelsHub/Qwen/Qwen3-32B \
+#   /ssd4/hyzhang/Kit/output_model/Ministral_to_Qwen3
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 mergekit-tokensurgeon \
+  /ssd4/hyzhang/MODELS/GLM-4-32B-0414 \
+  /ssd4/baohangli/ModelsHub/Qwen/Qwen3-32B \
+  /ssd4/hyzhang/Kit/output_model/GLM4_to_Qwen3
